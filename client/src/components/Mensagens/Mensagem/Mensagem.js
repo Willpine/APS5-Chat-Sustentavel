a7e966aa72e7444ff29c17d18d60c51ff432668d
1JeanCarlo2 <doritado@hotmail.com>
2020-05-08 21:05:15 -0300

    atualização hora msg enviada

--- client/src/components/Mensagens/Mensagem/Mensagem.js
+++ client/src/components/Mensagens/Mensagem/Mensagem.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 import './Mensagem.css';
 
+
 import ReactEmoji from 'react-emoji';
+let data = new Date();
+let horas = data.getHours();
+let minutos = data.getMinutes();
+let horaMsgEnviada = horas + ':' + minutos; 
 
 const Mensagem = ({ mensagem: { user, texto }, nome }) => {
     let mensagemUsuarioAtual = false;
@@ -16,13 +21,15 @@ const Mensagem = ({ mensagem: { user, texto }, nome }) => {
     return (
         mensagemUsuarioAtual ? (
             <div className="containerMensagem justifyEnd">
-                <p className="textoEnviado pr-10">{nomeFormatado}</p>
+                {/* <p className="textoEnviado pr-10">{nomeFormatado}</p> */}
                 <div className="mensagemRetangulo backgroundBlue">
                     <p className="mensagemTexto colorWhite">{ReactEmoji.emojify(texto)}</p>
                 </div>
+                <p className="hora pl-10">{horaMsgEnviada}</p>
             </div>
         ) : (
                 <div className="containerMensagem justifyStart">
+                    <p className="hora pr-10">{horaMsgEnviada}</p>
                     <div className="mensagemRetangulo backgroundLight">
                         <p className="mensagemTexto colorDark">{ReactEmoji.emojify(texto)}</p>
                     </div>
